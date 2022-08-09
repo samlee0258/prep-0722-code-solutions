269efe0959355efacd31eda376577e28b3e97b62
Samuel Lee <slee0258@gmail.com>
2022-08-09 08:40:51 +0000

    Completed prep JavaScript arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react',
+  'node', 'postgres'];
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('The first element of the array is:', firstElement);
+
+var thirdElement = languages[2];
+console.log('The 3rd element of the array is:', thirdElement);
+
+console.log('The length of the array is:', languages.length);
+
+var lastElement = languages[5];
+console.log('The last element of the array  is:', lastElement);
